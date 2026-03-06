763f99b0212cedbd5285b86f502b8f2075cde241
Gopikrish2k <gopikrish2k@gmail.com>
2026-03-06 13:04:32 +0530

    iris model deployment

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import joblib
+import numpy as np 
+
+model = joblib.load("iris_model.pkl")
+st.title("Iris Prediction")
+
+sepal_len = st.number_input("Sepal Length")
+sepal_wid = st.number_input("Sepal Width")
+petal_len = st.number_input("Petal Length")
+petal_wid = st.number_input("Petal Width")
+
+if st.button("predict"):
+    input_data = np.array([[sepal_len, sepal_wid, petal_len, petal_wid]])
+    prediction = model.predict(input_data)
+
+    flowers = ["Setosa", "Versicolor", "Virginica"]
+    st.success(f"prediction: {flowers[prediction[0]]}")
